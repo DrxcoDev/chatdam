4f2f2aa146fa9735ac47853a9c3e9d777c218c35
DrxcoDev <156522801>
2024-09-06 17:18:54 +0200

    ✨ New Commit

--- chatdam-fronted/src/components/UI/cards.jsx
+++ chatdam-fronted/src/components/UI/cards.jsx
@@ -1,15 +1,20 @@
 import './card.css'
 
-function Card({title, description}) {
+function Card({title, description, gradientX1=1, gradientX2=2}) {
     return (
         <div className="card">
-            <div className="card-container bg-black">
+            <div className="card-container">
                 <div className="p-10">
-                    <h1 className="text-neutral-200 text-4xl text-center">{title}</h1>
+                    <h1 className="text-white text-4xl text-center">{title}</h1>
                     <p className='mt-5 text-center text-neutral-200'>Using Chatdam is too easy for you envioriment</p>
                 </div>
             </div>
+
+
+
         </div>
+
+        
         
     );
 }
