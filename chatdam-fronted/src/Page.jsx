64db0bfbbf803997db26562cbe93261e1967bb88
DrxcoDev <156522801>
2024-09-06 17:09:42 +0200

    ✨ New Commit

--- chatdam-fronted/src/Page.jsx
+++ chatdam-fronted/src/Page.jsx
@@ -1,8 +1,27 @@
+import Card from './components/UI/cards'
 
 function App() {
   return (
     <main>
-      <h1 className="text-orange-600">Hola</h1>
+      <header className="flex mx-5 my-5 md:items-center">
+        <h1 className="text-2xl ml-3 font-medium">Chatdam</h1>
+        <div className="ms:ml-[50px] md:ml-[100px]">
+          <ul className="flex">
+            <li className="ms:mx-3 md:mx-5"><a href="#">Home</a></li>
+            <li className="ms:mx-3 md:mx-5"><a href="#">About</a></li>
+            <li className="ms:mx-3 md:mx-5"><a href="#">Contact</a></li>
+          </ul>
+        </div>
+
+      </header>
+
+      <div className="">
+        <div className="my-[150px]">
+          <Card title="Talk with everyone" description="Esto es una carta"/>
+        </div>
+        
+      </div>
+      
     </main>
     
   );
