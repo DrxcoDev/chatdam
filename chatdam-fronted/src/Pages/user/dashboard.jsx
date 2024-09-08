9705daa07e17b8f082ca9c1c670c0ea286d0a18a
DrxcoDev <156522801>
2024-09-08 13:36:42 +0200

    ✨ New Commit

--- chatdam-fronted/src/Pages/user/dashboard.jsx
+++ chatdam-fronted/src/Pages/user/dashboard.jsx
@@ -1,15 +1,32 @@
 // Dashboard.js
 import React from 'react';
 import { useUser } from './userContext.js'; // Importa el hook useUser
+import { signOut } from 'firebase/auth'; // Importa la función signOut de Firebase
+import { auth } from '../../firebase.js'; // Importa la instancia de auth de Firebase
+import { useNavigate } from 'react-router-dom'; // Importa el hook useNavigate
 
 function Dashboard() {
-  const { user } = useUser(); // Obtén el usuario del contexto
+  const { user, setUser } = useUser(); // Obtén el usuario y la función para actualizar el usuario del contexto
+  const navigate = useNavigate(); // Hook para navegar programáticamente
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('Usuario desconectado');
+        setUser(null); // Borra la información del usuario del contexto
+        navigate('/'); // Redirige a la página de inicio (Home) después de cerrar sesión
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div>
       <h1>Dashboard</h1>
       {user ? (
-        <p>Bienvenido, {user.displayName}</p> // Muestra el nombre del usuario
+        <div>
+          <p>Bienvenido, {user.displayName}</p> {/* Muestra el nombre del usuario */}
+          <button onClick={handleSignOut}>Cerrar sesión</button> {/* Botón para cerrar sesión */}
+        </div>
       ) : (
         <p>Cargando...</p>
       )}
